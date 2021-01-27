ba0f353fbdf8cd4c4c7a5f0fc6b5d48cf9af0ce4
jclot <julian.clot@iem.ed.cr>
2021-01-27 00:44:13 -0600

    Create a scroll, vertical calendar with all the months

---
+++ src/screens/Calendar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+ import {
+    Text,
+    View,
+    TextInput,
+    Platform,
+    Modal,
+    TouchableHighlight,
+    TouchableOpacity,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    ScrollView,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+
+export default function Calendars() {
+
+    return(
+
+    <View style={{backgroundColor: '#349beb'}}>
+
+        <CalendarList
+
+        onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+        pastScrollRange={0}
+        futureScrollRange={50}
+        scrollEnabled={true}
+        showScrollIndicator={true}
+        theme={{
+            backgroundColor: '#349beb',
+            calendarBackground: '#349beb',
+            todayTextColor: '#e3e156',
+            monthTextColor: '#e3e156',
+            dayTextColor: '#fff'
+        }}
+        style={{
+            marginTop: 20,
+            backgroundColor: '#349beb'
+        }}
+        />
+    </View>
+    )
+}
